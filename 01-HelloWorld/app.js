757743e696f902df3ada3d017bf57980a566d6c3
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-05 00:09:52 -0500

    feat: introduccion a variables y comentarios

--- 01-HelloWorld/app.js
+++ 01-HelloWorld/app.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+//console.log('Hello world');
+let a = 20; // Declare a variable local
+var b = 30; // Declare a variable global
+const c = 40; // Declare a constant
+
+//c = 50; // This will cause an error because 'c' is a constants
+
+console.log(a);
+a = 30;
+console.log(a + b);
