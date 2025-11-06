299dcc4465e3420d543706af7e2b80c119a4af4d
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-05 23:24:52 -0500

    primitivos en JS

---
+++ 01-HelloWorld/assets/js/primitives.js
@@ -0,0 +1,19 @@
+let name = 'Ricardo';
+let lastName = "Metzger";
+console.log(name + ' ' + lastName);
+console.log(`${name} ${lastName}`);
+
+let isMarried = false;
+console.log(`Is married? ${isMarried}`);
+
+let age = 31;
+console.log(`Age: ${age}`);
+
+let superPower;
+console.log(`Super Power: ${superPower}`);
+
+let iamNull = null;
+console.log(`Iam Null: ${iamNull}`);
+
+let symbolOne = Symbol('a');
+console.log(`Symbol: ${symbolOne}`);
