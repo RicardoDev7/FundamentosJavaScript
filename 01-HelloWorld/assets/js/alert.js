d8517c656245a7e847ce013fec39dae034510eae
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-05 23:01:59 -0500

    feat: alertas, prompt y confim en js

--- 01-HelloWorld/assets/js/alert.js
+++ 01-HelloWorld/assets/js/alert.js
@@ -1,1 +1,5 @@
-console.log(myName);
+alert('Hello world');
+let name = prompt('What is your name?', 'Someone');
+console.log('Hello ' + name);
+let isSure = confirm('Are you sure ' + name + '?');
+console.log('User is sure: ' + isSure);
