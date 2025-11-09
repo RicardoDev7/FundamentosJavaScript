7e44a6a180e0dbb7626b0394e81885a5080c42ff
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-08 23:30:33 -0500

    feat: asignaciones con operadores logicos

--- 01-HelloWorld/assets/js/bolean-logic.js
+++ 01-HelloWorld/assets/js/bolean-logic.js
@@ -24,3 +24,16 @@ console.log(returnTrue() || returnTrue());
 console.log(returnTrue() || returnFalse());
 console.log(returnFalse() || returnTrue());
 console.log(returnFalse() || returnFalse());
+
+console.warn('Asignation with logic operators');
+
+const iamUndefined = undefined;
+const iamNull = null;
+const iamFalse = false;
+
+const a1 = true && 'Hello World';
+const a2 = 'Hello' && 'World' && iamFalse && true;
+const a3 = iamFalse || 'I am not false';
+const a4 = iamFalse || iamUndefined || iamNull || 'Now I have a value' || true;
+const a5 = iamFalse || iamUndefined || returnTrue() || 'Now I have a value' || true;
+console.log({a1, a2, a3, a4, a5});
