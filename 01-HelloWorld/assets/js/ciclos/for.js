8433301389bc034179fc444b8118991eb819ae70
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-10 23:57:30 -0500

    feat: uso de for in en JS

--- 01-HelloWorld/assets/js/ciclos/for.js
+++ 01-HelloWorld/assets/js/ciclos/for.js
@@ -4,3 +4,8 @@ console.warn('--- Usando for tradicional ---');
 for (let i = 0; i < heroes.length; i++) {
     console.log(heroes[i]);
 }
+
+console.warn('--- Usando for in ---');
+for (let index in heroes) {
+    console.log(heroes[index]);
+}
