cca4e73d28325d195c1a66fb59030e791fb1ee6e
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-10 23:56:13 -0500

    feat: for tradicional en JS

---
+++ 01-HelloWorld/assets/js/ciclos/for.js
@@ -0,0 +1,6 @@
+const heroes = ['Batman', 'Superman', 'Wonder Woman', 'Flash', 'Aquaman'];
+
+console.warn('--- Usando for tradicional ---');
+for (let i = 0; i < heroes.length; i++) {
+    console.log(heroes[i]);
+}
