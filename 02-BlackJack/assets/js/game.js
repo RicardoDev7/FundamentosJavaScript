5a8db2dca968f36b2ae0bbdd4d7787f065be8da4
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-12 00:27:56 -0500

    feat: funcion de obtener valor de carta

--- 02-BlackJack/assets/js/game.js
+++ 02-BlackJack/assets/js/game.js
@@ -24,5 +24,12 @@ const askCard = () => {
         throw 'No cards in the deck';
     }
     let card = deck.pop();
-    return `${card}.png`;
+    return card;
 }
+
+const getCardValue = ( card ) => {
+    const valueCard = card.substring(0, card.length -1);
+    return ( isNaN( valueCard ) ) ? evaluateSpecialCard( valueCard ) : parseInt( valueCard );
+}
+
+const evaluateSpecialCard = ( valueCard ) => valueCard === 'A' ? 11 : 10;
