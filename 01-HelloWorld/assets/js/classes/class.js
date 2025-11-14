a48210116a990e82227991c7ec8d0fe4fbe3e94b
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-13 22:52:18 -0500

    feat: metodos en clases JS

--- 01-HelloWorld/assets/js/classes/class.js
+++ 01-HelloWorld/assets/js/classes/class.js
@@ -4,9 +4,19 @@ class People{
         this.code = code;
         this.message = message;
     }
+    amI(){
+        console.log(`Soy ${this.name} y mi identidad es ${this.code}`);
+    }
+    myMessage(){
+        console.log(`${this.code} dice ${this.message}`);
+    }
 }
 
 const spiderMan = new People('Peter Parker', 'Spiderman', 'Hello, I am Spiderman');
 const ironMan = new People('Tony Stark', 'Ironman', 'Hello, I am Ironman');
 console.log(spiderMan);
 console.log(ironMan);
+spiderMan.amI();
+spiderMan.myMessage();
+ironMan.amI();
+ironMan.myMessage();
