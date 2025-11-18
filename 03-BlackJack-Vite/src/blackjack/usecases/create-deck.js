d64d5221f92f8aa2281742037c4e15c95fd8b9a9
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-11-17 23:23:51 -0500

    feat: comments for methods in JS

--- 03-BlackJack-Vite/src/blackjack/usecases/create-deck.js
+++ 03-BlackJack-Vite/src/blackjack/usecases/create-deck.js
@@ -2,7 +2,17 @@ import { shuffle } from 'underscore';
 
 let deck = [];
 
+/**
+ * Creación de un deck
+ * @param {Array<string>} types - Ejemplo: ['C', 'D', 'H', 'S']
+ * @param {Array<string>} specials - Ejemplo ['A', 'J', 'Q', 'K']
+ * @returns {Array<string>}
+ */
 export const createDeck = (types, specials) => {
+    if(!types) throw new Error('Se requieren los tipos de cartas');
+    if(types.length === 0) throw new Error('Se requiere por lo menos un tipo de carta');
+    if(!specials) throw new Error('Se requieren los tipos especiales de cartas');
+    if(specials.length === 0) throw new Error('Se requiere por lo menos un tipo especial de carta');
     generateGeneralDeck(types);
     generateSpecialDeck(types, specials);
     return shuffle(deck);
